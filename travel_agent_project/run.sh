f864eccf271a645723e94099764f563eb802cd87
RohanSreejith <rohansreejith05@outlook.com>
2025-07-09 15:07:44 +0530

    Delete travel_agent_project directory

--- travel_agent_project/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Run Django app using Gunicorn
-gunicorn travel_agent_project.wsgi:application --bind 0.0.0.0:$PORT
--- travel_agent_project/travel_agent_app/apps.py
+++
@@ -1,9 +0,0 @@
-# travel_agent_app/apps.py
-from django.apps import AppConfig
-
-class TravelAgentAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'travel_agent_app'
-
-    def ready(self):
-        import travel_agent_app.signals  # This line is crucial
