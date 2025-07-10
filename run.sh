db58006b518447258455cec1c41442c717b8b3de
rohansreejith05 <rohansreejith05@gmail.com>
2025-07-10 09:41:36 +0530

    Add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-# Run Django app using Gunicorn
+# Exit on error
+set -o errexit
+
+python manage.py migrate --noinput
 gunicorn travel_agent_project.wsgi:application --bind 0.0.0.0:$PORT
